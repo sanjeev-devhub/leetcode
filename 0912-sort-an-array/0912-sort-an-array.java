9911bd092b08c7c85452bd3908c7b9fe1f503344
SANJEEV KUMAR <63458617+sanjeev-devhub@users.noreply.github.com>
2026-06-23 20:32:18 +0530

    Time: 1462 ms (6.32%), Space: 77.4 MB (92.29%) - LeetHub

--- 0912-sort-an-array/0912-sort-an-array.java
+++ 0912-sort-an-array/0912-sort-an-array.java
@@ -3,41 +3,37 @@ class Solution {
         mS(arr, 0, arr.length - 1);
         return arr;
     }
+
     private void mS(int[] arr, int low, int high) {
-        if (low == high) return;
-
+        if (low == high) {
+            return;
+        }
         int mid = (low + high) / 2;
-
         mS(arr, low, mid);
         mS(arr, mid + 1, high);
-
         merge(arr, low, mid, high);
-
     }
 
     private void merge(int[] arr, int low, int mid, int high) {
-        List<Integer> list = new ArrayList<>();
+        int[] temp = new int[arr.length];
         int left = low;
         int right = mid + 1;
+        int counter = 0;
         while (left <= mid && right <= high) {
-            if (arr[left] <= arr[right]) {
-                list.add(arr[left]);
-                left++;
+            if (arr[left] < arr[right]) {
+                temp[counter++] = arr[left++];
             } else {
-                list.add(arr[right]);
-                right++;
+                temp[counter++] = arr[right++];
             }
         }
         while (left <= mid) {
-            list.add(arr[left]);
-            left++;
+            temp[counter++] = arr[left++];
         }
         while (right <= high) {
-            list.add(arr[right]);
-            right++;
+            temp[counter++] = arr[right++];
         }
         for (int i = low; i <= high; i++) {
-            arr[i] = list.get(i - low);
+            arr[i] = temp[i - low];
         }
     }
 }
