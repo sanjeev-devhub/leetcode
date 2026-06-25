30d4e5793c19be188713af8c8331a507e98f4eef
SANJEEV KUMAR <63458617+sanjeev-devhub@users.noreply.github.com>
2026-06-26 01:24:13 +0530

    Time: 0 ms (100%), Space: 45.3 MB (15.99%) - LeetHub

---
+++ 3875-construct-uniform-parity-array-i/3875-construct-uniform-parity-array-i.java
@@ -0,0 +1,18 @@
+class Solution {
+    public boolean uniformArray(int[] nums1) {
+    //     int n=nums1[0]%2;
+    //     if(n==0){
+    //         return parityCheck(nums1,0);
+    //     }else{
+    //        return parityCheck(nums1,1);
+    //     }
+    // }
+    // private boolean parityCheck(int[] arr,int target){
+    //     for(int n:arr){
+    //         if(n%2!=target){
+    //             return false;
+    //         }
+    //     }
+        return true;
+    }
+}
