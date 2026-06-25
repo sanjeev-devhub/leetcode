99119ce8eab8b1d4b626b3162335df6ed5752c05
SANJEEV KUMAR <63458617+sanjeev-devhub@users.noreply.github.com>
2026-06-25 19:54:21 +0530

    Time: 2 ms (93.05%), Space: 43.6 MB (27.8%) - LeetHub

--- 1189-maximum-number-of-balloons/1189-maximum-number-of-balloons.java
+++ 1189-maximum-number-of-balloons/1189-maximum-number-of-balloons.java
@@ -1,23 +1,17 @@
 class Solution {
     public int maxNumberOfBalloons(String text) {
-        if(text.equalsIgnoreCase("loonbalxballpoonballoo")) return 2;
-        int[] freq= new int[26];
-        for(char c:text.toCharArray()){
-            freq[c-'a']= freq[c-'a']+1;
+        int[] freq = new int[26];
+
+        for (char c : text.toCharArray()) {
+            freq[c - 'a']++;
         }
-        char[] chars={'b','a','l','l','o','o','n'};
-        int count=0;
-        for(int i=0;i<chars.length;i++){
-            if(i==chars.length-1){
-                i=0;
-                count++;
-            }if(freq[chars[i]-'a']>0){
-                freq[chars[i]-'a'] =freq[chars[i]-'a']-1;
-            }else{
-                break;
-            }
-        }
-        return count;
-        
+
+        return Math.min(
+            Math.min(freq['b' - 'a'], freq['a' - 'a']),
+            Math.min(
+                Math.min(freq['l' - 'a'] / 2, freq['o' - 'a'] / 2),
+                freq['n' - 'a']
+            )
+        );
     }
 }
